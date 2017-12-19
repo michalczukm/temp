5f3f646b3ace6d70ba475cc687ae7484c9e646a8
Michal Michalczuk <michal.michalczuk@goyello.com>
2017-12-19 03:05:31 +0100

    wip

--- index.js
+++ index.js
@@ -2,19 +2,15 @@ const http = require('http');
 
 let counter = 0;
 
-const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  setTimeout(() => {
-    console.log('hit!');
     counter ++;
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end(`counter:${counter}`);
-  }, 2000);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at http://${''}:${port}/`);
 });
