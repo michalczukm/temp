70ea13ee3b80451b077ab23b52bf4f66b35f4b77
Michal Michalczuk <michal.michalczuk@goyello.com>
2017-12-19 03:00:00 +0100

    set port from params

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 let counter = 0;
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   setTimeout(() => {
